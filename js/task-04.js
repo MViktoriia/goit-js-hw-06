11e80cc344cd19e4717c43c208eeb87aeb3640a3
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-12 21:12:18 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
-const btnDecrement = 
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+let counterValue = 0;
+console.log(counterValue);
+
+const OnIncrementButtonClik = (event) => {
+     counterValue = counterValue + 1;
+        
+}; 
+
+const OnDecrementButtonClik = () => {
+    counterValue = counterValue - 1;
+};
+
+incrementBtn.addEventListener('click', OnIncrementButtonClik);
+decrementBtn.addEventListener('click', OnDecrementButtonClik);
+
+
