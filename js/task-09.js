221295aa92c2603433e5a3547600f46a8ad2b0c0
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-01-14 19:25:57 +0200

    task 9, 10(partualy)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const changeColorBtnEL = document.querySelector(".change-color");
+const colorSpanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+  
+changeColorBtnEL.addEventListener("click", onChangeColorBtnClickHandler);
+
+function onChangeColorBtnClickHandler(event) {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorSpanEl.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
+const namberInputEl = document.querySelector("input[type='number']");
+console.log(namberInputEl);
+const dataCreateButtonEl = document.querySelector("button[data-create]") 
+console.log(dataCreateButtonEl);
+const dataDestroyButtonEl = document.querySelector("button[data-destroy]") 
+console.log(dataDestroyButtonEl);
+const boxesEl = document.querySelector("div#boxes");
+console.log(boxesEl);
+
+dataCreateButtonEl.addEventListener("click", onDataCreateButtonClickHandler);
+dataDestroyButtonEl.addEventListener("click", onDataDestroyButtonClickHandler);
+
+function onDataCreateButtonClickHandler(event) {
+  createBoxes(namberInputEl.value);
+  
+}
+
+function onDataDestroyButtonClickHandler() {
+  destroyBoxes(boxesEl.children);
+  console.log(boxesEl.children);
+
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement("div");
+    console.log(boxEl);
+    const boxElSize = (30 + 10 * i) + 'px';
+    console.log(boxElSize);
+    boxEl.style.width = boxElSize;
+    boxEl.style.height = boxElSize;
+    boxEl.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(boxEl);
+  }
+  
+}
+
+function destroyBoxes(boxes) {
+
+  // for let box of boxes {
+  //   box.remove();
+  // }
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
